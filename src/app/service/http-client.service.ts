6344eba4b21124432b6675e5cba2bdcdbeea1dab
YuXing <a0975708860@gmail.com>
2021-10-17 15:56:38 +0800

    feat : create call backend api

--- src/app/service/http-client.service.ts
+++ src/app/service/http-client.service.ts
@@ -1,15 +1,26 @@
+import { BaseResponse } from './../model/BaseResponse';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { BaseRequest } from '../model/BaseRequest';
+import { map, pluck } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
 export class HttpClientService {
-  apiurl: string = '';
   constructor(private http: HttpClient) {}
 
-  getProduct(): Observable<any>{
-    return this.http.post("http://localhost:8081/research",null);
+  postApi(apiUrl: string, req: any): Observable<any> {
+    return this.http
+      .post('http://localhost:8081/' + apiUrl, this.getReq(req))
+      .pipe(pluck('result'));
+  }
+
+  getReq(req: any): BaseRequest {
+    return {
+      reqAccount: '',
+      reqTime: '',
+      body: req,
+    };
   }
 }
